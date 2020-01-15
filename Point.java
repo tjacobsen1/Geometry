0fe105c769d99fd2574a2f5ec98fa53696ff0fbd
Thomas Saether Jacobsen <jacobsents@s.dcsdk12.org>
2020-01-15 08:48:50 -0700

    Started Point class

--- Point.java
+++ Point.java
@@ -1,3 +1,15 @@
 public class Point {
     
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x  = x;
+        this.y = y;
+    }
+
+    public int calculateDistance() {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
 }
