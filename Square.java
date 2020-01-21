f13d47e4b0bbbd76646f557e8cf7efd18e27d77f
Ben McMann <bbmcmann@volosys.com>
2020-01-21 08:25:27 -0700

    Begin x collision test

--- Square.java
+++ Square.java
@@ -1,26 +1,40 @@
 class Square{
 
-    private Point pt1;
-    private Point pt2;
-    private Point pt3;
-    private Point pt4;
+    private Point bottomLeft;
+    private Point topLeft;
+    private Point topRight;
+    private Point bottomRight;
+    private int sideLen;
+    private Point[] points;
 
-    public Square(Point pt1, Point pt2, Point pt3, Point pt4){
-        this.pt1 = pt1;
-        this.pt2 = pt2;
-        this.pt3 = pt3;
-        this.pt4 = pt4;
+    public Square(Point pt1, int sideLen){
+        this.points[0] = pt1;                                                       //bottom left
+        this.points[1] = new Point(pt1.getX(), pt1.getY() + sideLen);               //top left
+        this.points[2] = new Point(pt1.getX() + sideLen, pt1.getY() + sideLen);     //top right
+        this.points[3] = new Point(pt1.getX() + sideLen, pt1.getY());               //bottom right
+        this.sideLen = sideLen;
+        
     }
 
-    private int findSide(){
-        return pt1.getX() - pt2.getX();
+    public Point getPoint(int x){
+        return points[x];
     }
 
-    public double findArea(){
-        return findSide() * findSide();
+    public int findArea(){
+        return sideLen * sideLen;
     }
 
-    
+    public boolean collision(Square sq2){
+        for(int i = 0; i < 4; i++){
+            if((points[i].getX() < sq2.getPoint(0).getX() && points[i].getX() > sq2.getPoint(2).getX())){
+
+            }
+        }
+        
+
+
+        return true;
+    }
 
 
 }
